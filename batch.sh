83b4fb8242cb16ab613f042d85d39c5631ca0dff
Dody <Dodysenz@gmail.com>
2023-05-16 13:59:13 +0800

    update bash code

--- batch.sh
+++ batch.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-cat ./links.txt | xargs -P 2 -I % sh -c './dezoomify-rs/dezoomify-rs -l %'
+cat ./links.txt | xargs -P 4 -I % sh -c './dezoomify-rs/dezoomify-rs -l %'
 mv *.jpg ./images
 
 # use this to create a symbolic link
